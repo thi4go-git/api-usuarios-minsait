cb20394b68bf68f8dacf70e3d2f52b021fbb4a9f
Thiago junior <thi4go19@gmail.com>
2024-07-25 23:07:28 -0300

    Commit inicial

---
+++ src/test/java/com/dynns/cloudtecnologia/api/ApiUsuariosMinsaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynns.cloudtecnologia.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuariosMinsaitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
